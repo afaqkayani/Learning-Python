910cc9fd6beed8b0b8c1874986646fffe32a4e1a
Afaq Kayani <muhammadafaqkayani@gmail.com>
2024-11-26 19:26:17 +0500

    Simple Calculator
    Simple Calculator using python .
    Arithmetic operations.

---
+++ calculator.py
@@ -0,0 +1,6 @@
+a=10
+b=4
+print("Sum of ",a," and ",b," is = ",a+b)
+print("Substraction of ",a," and ",b," is = ",a-b)
+print("Multiplication of ",a," and ",b," is = ",a-b)
+print("Division of ",a," and ",b," is = ",a/b)
