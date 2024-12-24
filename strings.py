c6421b5502ed46f6c2178478b5a1eae01053eca0
Afaq Kayani <muhammadafaqkayani@gmail.com>
2024-12-24 19:48:30 +0500

    Strings
    String and how to use it

---
+++ strings.py
@@ -0,0 +1,22 @@
+name="afaq"
+friend="zeeshan"
+anotherfriend="khurram"
+
+abc="""Hello said, \"I need a nap
+what are u doing
+i am doing good 
+good good"""
+
+print("Hello", name ,"\n")
+
+print("Prints name by index")
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3],"\n")
+#print(name[4]) throws an eeeor
+ 
+print(abc,"\n")
+print("lets use a foor loop\n")
+for ch in abc:
+    print(ch)
